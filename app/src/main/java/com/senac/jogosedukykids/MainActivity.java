4a333843aa9c6bc4440641ec7037f3c6a4861fe9
Leonardo Valadão <leonardovo.iel@fieg.com.br>
2022-12-05 23:22:20 -0300

    2ª Versão do Projeto Integrador 4.

--- app/src/main/java/com/senac/jogosedukykids/MainActivity.java
+++ app/src/main/java/com/senac/jogosedukykids/MainActivity.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_jogoMatematica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent telaJogoMatematica = new Intent(this, JogoMatematica.class);
-        startActivity(telaJogoMatematica);
+        btn_jogoMatematica = findViewById(R.id.btn_jogoMatematica);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btn_jogoMatematica) {
+            Intent telaJogoMatematica = new Intent(this, JogoMatematica.class);
+            startActivity(telaJogoMatematica);
+        }
     }
 }
