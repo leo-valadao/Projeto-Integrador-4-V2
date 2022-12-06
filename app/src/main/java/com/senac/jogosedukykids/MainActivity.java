502c287b9764686919afde45007c09eab8dd923b
Leonardo Valadão <leonardovo.iel@fieg.com.br>
2022-12-05 23:24:32 -0300

    3ª Versão do Projeto Integrador 4.

--- app/src/main/java/com/senac/jogosedukykids/MainActivity.java
+++ app/src/main/java/com/senac/jogosedukykids/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,9 +15,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         btn_jogoMatematica = findViewById(R.id.btn_jogoMatematica);
+        btn_jogoMatematica.setOnClickListener(this);
     }
 
     @Override
